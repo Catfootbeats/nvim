6fa7a48ab6d51637efbd3da49e3d4b3a1a4d0f28
Catfootbeats <mr.catfootbeats@outlook.com>
2024-12-28 21:54:30 +0800

    change size of toggletetm

--- lua/config/toggleterm.lua
+++ lua/config/toggleterm.lua
@@ -4,7 +4,7 @@ require("toggleterm").setup {
     if term.direction == "horizontal" then
       return 10
     elseif term.direction == "vertical" then
-      return vim.o.columns * 0.4
+      return vim.o.columns * 0.3
     end
   end,
   open_mapping = [[<C-t>]],
